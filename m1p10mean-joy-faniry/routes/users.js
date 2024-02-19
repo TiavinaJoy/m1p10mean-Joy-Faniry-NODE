fe3ceb46beb9724ed154ec45b0bafaf04bfe232a
Faniriniaina7 <faniryrandriamparany@gmail.com>
2024-02-19 15:53:19 +0300

    Liste des roles

--- m1p10mean-joy-faniry/routes/users.js
+++ m1p10mean-joy-faniry/routes/users.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var clientController = require("../src/controller/clientController");
+var roleController = require("../src/controller/roleController");
 var { authenticateClientToken } = require("../src/middleware/clientMiddleware");
 
 /* GET users listing. */
 router.get('/user', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/role',roleController.listeRoles);
 
 router.post('/client', clientController.register);
 
