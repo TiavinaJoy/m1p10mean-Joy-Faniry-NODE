6506a04bf80d926445f0a37588646ecbcbd92b5b
TiavinaJoy <96420703+TiavinaJoy@users.noreply.github.com>
2024-02-26 18:39:22 +0300

    creation model Depense

---
+++ m1p10mean-joy-faniry/src/model/depense.js
@@ -0,0 +1,27 @@
+const {mongoose } = require("../configuration/database");
+
+ 
+const depenseSchema = new mongoose.Schema({
+  intitule: {
+    type: String,
+    required: [true,"L'intitule de la dépense est obligatoire."]
+  },
+  type:{
+    type: String,
+    enum:['Achat pièce','Autre dépense','Loyer']
+  },
+  montant:{
+    type: Number,
+    min:[1,'Une dépense doit être supérieur à 0']
+  },
+  datePaiement:{
+    type: Date,
+    default:new Date()
+  }
+});
+
+depenseSchema.set('timestamps',true);
+
+const depense = mongoose.model('depense', depenseSchema,'depense');
+
+module.exports =  depense;
