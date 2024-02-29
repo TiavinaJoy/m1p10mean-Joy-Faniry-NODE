bd05d912c8794a4d4cb4f46b2d80e2fbb912e7ac
Faniriniaina7 <faniryrandriamparany@gmail.com>
2024-02-29 22:09:51 +0300

    modification timezonedate

--- m1p10mean-joy-faniry/src/helper/DateHelper.js
+++ m1p10mean-joy-faniry/src/helper/DateHelper.js
@@ -1,12 +1,15 @@
 function timezoneDateTime(dateString) {
-    const daty = new Date(dateString);
-    const part1 = dateString.split(' ')[1];
-    const hourString = part1.split(':')[0];
-    const hour = parseInt(hourString);
-    daty.getHours() == hour ? true : daty.setHours(daty.getHours() + 3);
-    return daty;
-}
-
+        const daty = new Date(dateString);
+        const part1 = dateString.split(' ')[1];
+        const hourString = part1.split(':')[0];
+        const hour = parseInt(hourString);
+        daty.setHours(daty.getHours() +3);
+    
+        if(daty.getUTCHours() != hour ){
+            daty.setUTCHours(daty.getUTCHours() + 3);
+        }
+        return daty;
+    }
 module.exports = {
     timezoneDateTime
 }
